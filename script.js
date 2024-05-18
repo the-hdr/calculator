5cbb6de0b9e56a3b791bfcd21844a1da30c20989
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-05-18 20:40:44 +0530

    update files
    updated all files

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 let previousAnswer = "";
-let currentExpression = "";
+
+let isEmpty = true;
+let currentExpression = "0.00";
 
 let operatorUsed = false;
 
 let buttons = document.querySelectorAll (".button");
 
-function extendExpression (event)
-{
-    let buttonId = event.target.id;
-    let buttonClass = event.target.className;
-
-    //alert ("ID: " + buttonId);
-}
+updateCurrentExpression (currentExpression);
 
 buttons.forEach (function (button) {
     button.addEventListener ("click", function (event) {
         extendExpression (event);
     });
-})
+});
+
+function extendExpression (event)
+{
+    let triggeringButtonId = event.target.id;
+    let triggeringButtonClass = event.target.className;
+
+    if (triggeringButtonClass == "clear-button")
+    {
+        currentExpression = "0.00";
+        previousAnswer = "0.00";
+    }
+    else if (triggeringButtonClass == "delete-button")
+    {
+        if (currentExpression.length == 1)
+        {
+            currentExpression = "0.00";
+        }
+        else
+        {
+            currentExpression = currentExpression.substring (0, currentExpression.length - 1);
+        }
+    }
+
+    // continue from here;
+}
+
+function updateCurrentExpression (newExpression)
+{
+    // update currentExpression:
+    currentExpression = newExpression;
+
+    let currentExpressionDiv = document.getElementById ("current-result");
+    currentExpressionDiv.innerHTML = currentExpression;
+}
